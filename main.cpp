e9c01e2f187038e71f76e8422acebd54a02a27e6
James Skön <skonjp@kenyon.edu>
2025-01-30 15:36:48 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 
 using namespace std;
 
+static int c = 0;
+
 // Fund to compute the fibonacci number of n
 int fibonacci(int n) {
+    c+=1;
     if (n <= 1) {
         return n;
     }
@@ -16,6 +19,7 @@ int main() {
     cout << "Enter a number: ";
     cin >> n;
     cout << fibonacci(n) << endl;
+    cout << "Number of recursive calls:" << c << endl;
 
     return 0;
 }
